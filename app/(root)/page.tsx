518de674d88ab567ace31acc494c6f13a4db80de
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-09-02 10:42:55 -0600

    feat: add HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className='home-header'>
+                <HeaderBox 
+                    type="greeting"
+                    title="Bienvenido"
+                    user={ 'Invitado'}
+                    subtext="Accede y maneja tu cuenta con transaciones eficaces."
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
