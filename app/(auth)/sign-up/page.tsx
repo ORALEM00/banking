a4055142dd95807857ae0129f4914b15ae7f1514
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-09-06 14:44:04 -0600

    feat: add SignUp

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignOut = () => {
+const SignUp = () => {
   return (
-    <div>SignOut</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" /> 
+    </section>
   )
 }
 
-export default SignOut
+export default SignUp
