cddc67250f1a17ddbcc41492e8ff38be2d9924b1
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-09-13 12:11:18 -0600

    feat: add recent transactions

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -14,12 +14,12 @@ const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
   const data = {
     datasets: [
       {
-        label: 'Bancos',
-        data: [1250,2500,3750],
+        label: 'Banks',
+        data: balances,
         backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'] 
       }
     ],
-    labels: ['MercadoPago','Santander','CapitalOne']
+    labels: accountNames
   }
 
   return <Doughnut 
