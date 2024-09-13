ea36070b6fa86af7760b639d72bf2b3218f58b20
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2024-09-13 12:59:26 -0600

    feat: add myBanks

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,7 +15,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Footer from "./Footer"
-
+import PlaidLink from './PlaidLink'
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -70,7 +70,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                 )
               })}
 
-            
+              <PlaidLink user={user} />
               </nav>
             </SheetClose>
 
